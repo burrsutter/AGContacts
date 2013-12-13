d7caafec4539a9bf23d36836760e6af3779fe7eb
Burr Sutter <burrsutter@gmail.com>
2013-12-13 13:24:54 -0500

    initial load into github

---
+++ README.md
@@ -0,0 +1,17 @@
+This contains the demo code seen in this video
+https://vimeo.com/81565404
+
+Now using the AerogearJS API intead of vanilla jQuery
+hoping to show off full CRUD
+C - Create/POST
+R - Read/GET
+U - Update/PUT
+D - Delete/DELETE
+
+A very simple application to demonstrate how to develop Apache Cordova/Phonegap apps in 
+
+JBoss Tools/JBoss Developer Studio.  
+
+Since the recording of the video, I have also added the Console plugin in addition to the 
+Contacts and Motion plugins.
+
---
+++ www/js/index.js
@@ -0,0 +1,93 @@
+var memberPipe  = AeroGear.Pipeline([{
+        name: "members",
+        settings: {
+            baseURL: "http://agmobile-html5.rhcloud.com/rest/forge/"
+        }
+    }
+ ]).pipes.members;
+
+
+var app = {
+	initialize : function() {
+		this.bind();
+	},
+	bind : function() {
+		document.addEventListener('deviceready', this.deviceready, false);
+	},
+	onAccSuccess : function(acceleration) {
+		$("#accX").html('Acceleration X: ' + acceleration.x);
+		$("#accY").html('Acceleration Y: ' + acceleration.y);
+		$("#accZ").html('Acceleration Z: ' + acceleration.z);
+	},
+	onAccError : function() {
+		alert('ACC Error');
+	},
+	onContactsSuccess: function(contacts) {
+	    $("#debug").html("Found: " + contacts.length);
+	    devContactsLV = $("#deviceContacts"); 
+	    devContactsLV.empty();
+	    var items = [];
+	    for (var i = 0; i < contacts.length; i++) {
+	    	items.push("<li><a href='#'>" + contacts[i].displayName + "</a></li>");
+	    }
+	    devContactsLV.append(items);
+	    devContactsLV.listview("refresh");	
+	},
+	onContactsError: function() {
+	    $("#debug").html("error...");
+	},	
+	deviceready : function() {
+		// note that this is an event handler so the scope is that of the event
+		// so we need to call app.report(), and not this.report()
+		// app.report('deviceready');
+		console.log("deviceready1");
+		navigator.accelerometer.watchAcceleration(app.onAccSuccess, app.onAccError, {frequency:100});
+
+		/*
+		$.getJSON("http://agmobile-html5.rhcloud.com/rest/forge/members", function(data) {
+			$("#contacts").empty();
+		    var items = [];
+		    $.each(data, function(key, val) {
+		    	console.log("item: " + key + " " + val.name);
+		    	items.push("<li><a href='#"+ key + "'>" + val.name +"</a></li>");
+		    });
+		    $("#contacts").append(items);
+		    $("#contacts").listview("refresh");
+		});    	
+		*/
+		memberPipe.read({
+	        success: function( members ) {
+	        	console.log("data: " + members);
+	            contactsLV = $("#contacts"); 
+	        	contactsLV.empty();
+			    var items = [];
+			    $.each(members, function(memberId) {
+			    	console.log(memberId + " " + members[memberId].name);
+			    	items.push("<li><a href='#"+ memberId + "'>" + members[memberId].name +"</a></li>");
+			    });
+			    contactsLV.append(items);
+			    contactsLV.listview("refresh");	        	
+	        }
+	    });
+		
+		$("#btnDeviceContacts").on("click", function(e) {
+			   console.log("button clicked, going to fetch local contacts");
+			   $("#debug").html("finding...");
+			   var options      = new ContactFindOptions();
+			   options.multiple = true;
+			   var fields       = ["displayName", "name"];
+			   navigator.contacts.find(fields, app.onContactsSuccess, app.onContactsError, options);
+		});
+
+		$("#btnAddContact").on("click", function(e) {
+			  console.log("add contact");
+		});
+	},
+	report : function(id) {
+		console.log("report:" + id);
+		// hide the .pending <p> and show the .complete <p>
+		document.querySelector('#' + id + ' .pending').className += ' hide';
+		var completeElem = document.querySelector('#' + id + ' .complete');
+		completeElem.className = completeElem.className.split('hide').join('');
+	}
+};
